92935c5ab3e2ce54a4de5a3079e1f323423f2646
Graziel Sorita <grzlsrt@gmail.com>
2023-04-18 10:24:25 +0800

    commit

--- streamlit/hr-attrition.py
+++ streamlit/hr-attrition.py
@@ -1,13 +1,17 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-from sklearn.impute import SimpleImputer
-import pickle
+import streamlit as st
 
 st.set_page_config(page_title='HR Attrition')
 
 def load_data():
   attrition_data = pd.read_csv("IBM_HR-Attrition.csv")
   return data
-  
 
+
+
+def load_data():
+    data = pd.read_csv(
+        "./data/ph_data.csv",
+        encoding='ISO-8859-1'
+    )
+    return data

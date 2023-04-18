d1a7588ebe9051a6164a9459b5333186ba4bceb3
Graziel Sorita <grzlsrt@gmail.com>
2023-04-19 04:27:12 +0800

    commit

--- streamlit/hr-attrition.py
+++ streamlit/hr-attrition.py
@@ -4,14 +4,13 @@ import streamlit as st
 st.set_page_config(page_title='HR Attrition')
 
 def load_data():
-  attrition_data = pd.read_csv("IBM_HR-Attrition.csv")
-  return data
-
-
-
-def load_data():
     data = pd.read_csv(
-        "./data/ph_data.csv",
-        encoding='ISO-8859-1'
+        "IBM_HR-Attrition.csv"
     )
     return data
+
+def introduction():
+    data = load_data()
+    with st.expander("View Data"):
+        st.dataframe(data)
+        st.caption("*Source: IBM HR Analytics Employee Attrition & Performance*")
